a62dc6cd2a87ca4e6324cc7a1b56411894e0d8b8
Shilpashreeau <shilpashreehebbar@gmail.com>
2023-04-13 13:47:48 -0400

    created get and post routes

--- server.js
+++ server.js
@@ -8,3 +8,41 @@ const Flight=require("./models/Flight");
 
 const app=express();
 
+//*  setting the middleware
+app.set("view engine", "jsx");
+app.engine("jsx", require("jsx-view-engine").createEngine());
+app.use((req, res, next) => {
+    console.log("I run for all routes");
+    next();
+  });
+  app.use(express.urlencoded({ extended: false }));
+
+
+  //* query all documents and return the selected fields
+  app.get("/flights", (req, res) => {
+    console.log(req);
+   
+    Flight.find({},(error,allFlights)=>{
+    res.render('Index',{flights:allFlights})
+    })
+  });
+//* post flights
+
+  app.post("/flights", (req, res) => {
+    console.log(req.body);
+   
+ 
+   
+    Flight.create(req.body, (error,createdFlight)=>{
+      
+    
+      res.redirect('/flights');
+    
+    })
+    
+  });
+
+  app.listen(3000, () => {
+    console.log("Server is up!");
+    connectToDB();
+  });
